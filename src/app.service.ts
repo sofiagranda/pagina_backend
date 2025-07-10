f091581adf52287dcc5e6e33364c4a91fd2da6ca
copa-los-de-solanda <cfm.10c.nicolasparedes@gmail.com>
2025-07-10 12:16:47 -0500

    cicd

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hola bienvenivdo al Torneo Copa Los De Solanda';
+    return 'Hola bienvenido al Torneo Copa Los De Solanda';
   }
 }
